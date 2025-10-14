70a78d20b65fc651df8a747d650916f03a270131
Giovanne <giovannemg22@hotmail.com>
2025-10-13 22:11:43 -0300

    Criando BD

---
+++ script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bdprojeto;
+
+use bdprojeto;
+
+CREATE TABLE tbPedido(
+codPedido int primary key auto_increment,
+tipoJogo varchar(50),
+valorJogo varchar(50),
+valorOpcao varchar(50),
+valorTotal varchar(50)
+);
+
+
